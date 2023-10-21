1f29a7bbac4f5a4f3936f74d21958c49876c1b7e
Axel Buob <buobaxel@gmail.com>
2023-10-21 23:45:25 +0200

    lesson 31

---
+++ section_4/reaction-timer/src/components/Block.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <h1>Ninja reaction timer</h1>
+  </template>
+  
+  <script>
+    export default {
+      name: 'App',
+      components: {
+  
+      }
+    }
+  </script>
+  
+  <style>
+    
+  </style>
